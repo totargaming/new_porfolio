151cc2216796665ffb18676c50305c06d99de963
totargaming <totargaming@gmail.com>
2024-12-06 15:12:26 +1030

    Update GitHub link and enhance resume download button functionality

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -1,17 +1,18 @@
-import { cn } from '@/lib/utils'
-import { Download } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-import { buttonVariants } from './ui/button'
-import HackerBtn from './animation/HackerBtn'
+import { Download } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+import HackerBtn from './animation/HackerBtn';
 
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-  
-    <HackerBtn label='Download Resume' />
-  </div>
-  )
+      <Link href="/resume.pdf" passHref legacyBehavior>
+        <a download>
+          <HackerBtn label="Download Resume" />
+        </a>
+      </Link>
+    </div>
+  );
 }
 
-export default DownLoadResumeBtn
+export default DownLoadResumeBtn;
